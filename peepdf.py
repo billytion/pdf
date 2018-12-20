61feb17ffa4b8e5460903e9fc271c2afedcc71fa
billytion <45887436+billytion@users.noreply.github.com>
2018-12-20 19:28:16 +0100

    Update peepdf.py

--- peepdf.py
+++ peepdf.py
@@ -36,7 +36,7 @@ import hashlib
 import traceback
 import json
 from datetime import datetime
-from PDFCore import PDFParser, vulnsDict, PDFIndirectObject, PDFBody
+from PDFCore import *
 from PDFUtils import vtcheck
 
 
