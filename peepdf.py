8b8e5ce726fc93fc1c286612171d4703e08be253
billytion <45887436+billytion@users.noreply.github.com>
2018-12-20 19:58:16 +0100

    Update peepdf.py

--- peepdf.py
+++ peepdf.py
@@ -155,8 +155,8 @@ def getPeepXML(statsDict, version, revision):
         objects = etree.SubElement(versionInfo, 'objects', num=statsVersion['Objects'][0])
         for id in statsVersion['Objects'][1]:
             object = etree.SubElement(objects, 'object', id=str(id))
-            if pdfObject.hasElement('/Type'):
-                typeObject = pdfObject.getElementByName('/Type')
+            if object.pdfObject.hasElement('/Type'):
+                typeObject = object.pdfObject.getElementByName('/Type')
                 object.set('type', typeObject)
             if statsVersion['Compressed Objects'] is not None:
                 if id in statsVersion['Compressed Objects'][1]:
